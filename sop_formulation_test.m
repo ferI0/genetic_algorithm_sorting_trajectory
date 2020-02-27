b96aa139704489953935085088b6381040a47db1
Fabian Erichsmeier <fabian.erichsmeier@fh-bielefeld.de>
2020-02-27 14:24:37 +0100

    Generation of random seedlings for 4 positions with two target groups and calculation of length

--- sop_formulation_test.m
+++ sop_formulation_test.m
@@ -1,10 +1,14 @@
+%% Test of route generation for genetic algorithm
+%% FE 2020
+
+%% Mostly manually set start and end positions with random target groups for each seedling
 n = 4;
-vertices = zeros(1,2*n+1);
+vertices = 2*n+1;
 startEnd = [0 0];
-seedlings = [1 1 1,
-             1 2 2,
-             2 1 1,
-             2 2 2];
+seedlings = [1 1 randi([1 2],1),
+             1 2 randi([1 2],1),
+             2 1 randi([1 2],1),
+             2 2 randi([1 2],1)];
 target1 = [3 2,
            3 1,
            4 2,
@@ -14,12 +18,30 @@ target2 = [5 2,
            5 1,
            6 2,
            6 1];
-       
-%% Manual route selection
-vertices = [startEnd; seedlings(1,1:2); target1(1,:); seedlings(2,1:2); target2(1,:); startEnd]
+%% Generate population an route vector depending on population       
+population = randperm(n,n);
+routeMatrix = zeros(vertices, 2);
+posCount = 2;
+targetCount1 = 1;
+targetCount2 = 1;
+for i = 1:length(population)
+    routeMatrix(posCount,:) = seedlings(population(i),1:2);
+    if seedlings(population(i),3) == 1
+        routeMatrix(posCount+1,:) = target1(targetCount1,:);
+        targetCount1 = targetCount1 + 1;
+    end
+    if seedlings(population(i),3) == 2
+        routeMatrix(posCount+1,:) = target2(targetCount2,:);
+        targetCount2 = targetCount2 + 1;
+    end
+    posCount = posCount + 2;
+end
+
+%% Calculation of route length
 routeLength = 0;
-for i = 2:length(vertices)
-    verticeLength = abs(sqrt(vertices(i,1)^2+vertices(i,2)^2) - sqrt(vertices(i-1,1)^2+vertices(i-1,2)^2))
+for i = 2:vertices
+    verticeLength = abs(sqrt(routeMatrix(i,1)^2+routeMatrix(i,2)^2)...
+        - sqrt(routeMatrix(i-1,1)^2+routeMatrix(i-1,2)^2))
     routeLength = routeLength + verticeLength
 end
            
