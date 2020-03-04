393886cbb6f7de6687a4159f0304229aa29895b0
Fabian Erichsmeier <fabian.erichsmeier@fh-bielefeld.de>
2020-03-04 17:55:21 +0100

    STarting the iteration stuff

--- sop_formulation_test.m
+++ sop_formulation_test.m
@@ -6,52 +6,63 @@ n = 4;
 vertices = 2*n+1;
 startEnd = [0 0];
 seedlings = [1 1 randi([1 2],1),
-             1 2 randi([1 2],1),
-             2 1 randi([1 2],1),
-             2 2 randi([1 2],1)];
+    1 2 randi([1 2],1),
+    2 1 randi([1 2],1),
+    2 2 randi([1 2],1)];
 target1 = [3 2,
-           3 1,
-           4 2,
-           4 1];
-       
+    3 1,
+    4 2,
+    4 1];
+
 target2 = [5 2,
-           5 1,
-           6 2,
-           6 1];
-%% Generate population an route vector depending on population       
-population = randperm(n,n);
-routeMatrix = zeros(vertices+1, 2);
-posCount = 2;
-targetCount1 = 1;
-targetCount2 = 1;
-for i = 1:length(population)
-    routeMatrix(posCount,:) = seedlings(population(i),1:2);
-    if seedlings(population(i),3) == 1
-        routeMatrix(posCount+1,:) = target1(targetCount1,:);
-        targetCount1 = targetCount1 + 1;
+    5 1,
+    6 2,
+    6 1];
+
+%% IterCount and "best" Solution so far
+currentRecord = 999999;
+maxIter = 10;
+iterCount = 0;
+while iterCount < maxIter
+    %% Generate population an route vector depending on population
+    population = randperm(n,n);
+    routeMatrix = zeros(vertices+1, 2);
+    posCount = 2;
+    targetCount1 = 1;
+    targetCount2 = 1;
+    for i = 1:length(population)
+        routeMatrix(posCount,:) = seedlings(population(i),1:2);
+        if seedlings(population(i),3) == 1
+            routeMatrix(posCount+1,:) = target1(targetCount1,:);
+            targetCount1 = targetCount1 + 1;
+        end
+        if seedlings(population(i),3) == 2
+            routeMatrix(posCount+1,:) = target2(targetCount2,:);
+            targetCount2 = targetCount2 + 1;
+        end
+        posCount = posCount + 2;
     end
-    if seedlings(population(i),3) == 2
-        routeMatrix(posCount+1,:) = target2(targetCount2,:);
-        targetCount2 = targetCount2 + 1;
+    %% Calculate Fitness
+    %% Calculation of route length
+    routeLength = 0;
+    for i = 2:vertices
+        verticeLength = abs(sqrt(routeMatrix(i,1)^2+routeMatrix(i,2)^2)...
+            - sqrt(routeMatrix(i-1,1)^2+routeMatrix(i-1,2)^2));
+        routeLength = routeLength + verticeLength;
     end
-    posCount = posCount + 2;
+    
+    %% Plot route
+    figure(1);
+    xlim([0 6]);
+    ylim([0 3]);
+    grid on;
+    for i = 1:vertices
+        hold on
+        plot(routeMatrix([i, i+1],1), routeMatrix([i, i+1],2))
+        hold off
+    end
+    
+    %% Increment iter
+    iterCount = iterCount+1;
 end
 
-%% Calculation of route length
-routeLength = 0;
-for i = 2:vertices
-    verticeLength = abs(sqrt(routeMatrix(i,1)^2+routeMatrix(i,2)^2)...
-        - sqrt(routeMatrix(i-1,1)^2+routeMatrix(i-1,2)^2));
-    routeLength = routeLength + verticeLength;
-end
-
-%% Plot route
-figure(1);
-xlim([0 6]);
-ylim([0 3]);
-grid on;
-for i = 1:vertices
-    hold on
-    plot(routeMatrix([i, i+1],1), routeMatrix([i, i+1],2))
-    hold off
-end
